5db9064404b152059bc476da15c6a484ed17ddd0
krishna1484 <krishna.jajula@gmail.com>
2023-03-10 13:30:49 -0600

    add junit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
     @Test
     void contextLoads() {
+        logger.info("In ContextLoads test start");
+        assertEquals(true,true);
     }
 
 }
