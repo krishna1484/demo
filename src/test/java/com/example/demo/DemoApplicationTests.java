b643f42d2fbc25f5f8a32babe4a19bfc64490458
krishna1484 <krishna.jajula@gmail.com>
2023-03-10 15:38:14 -0600

    make junit test fail

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -15,7 +15,7 @@ class DemoApplicationTests {
     void contextLoads() {
         logger.info("In ContextLoads test start");
         logger.info("In ContextLoads test start second time");
-        assertEquals(true,true);
+        assertEquals(true,false);
     }
 
 }
