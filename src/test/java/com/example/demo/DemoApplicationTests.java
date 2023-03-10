c10dd645828f1d72f3f92f4ef83fd3d432baef3a
krishna1484 <krishna.jajula@gmail.com>
2023-03-10 15:35:10 -0600

    add junit test log statement

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -14,6 +14,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         logger.info("In ContextLoads test start");
+        logger.info("In ContextLoads test start second time");
         assertEquals(true,true);
     }
 
